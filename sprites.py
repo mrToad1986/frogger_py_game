95baa1c406932719503c5c7f5123b4fdbc74f1bf
mrToad1986 <korostylev.ov@gmail.com>
2026-09-23 22:01:26 +0300

    add collision check to sprites.py

--- sprites.py
+++ sprites.py
@@ -8,6 +8,7 @@ class BaseSprite(pygame.sprite.Sprite):
         super().__init__()
         self.image = image
         self.rect = self.image.get_rect(topleft=pos)
+        # self.mask = pygame.mask.from_surface(self.image)
         self.type = type
 
     def draw(self, surface):
@@ -27,3 +28,13 @@ class MovingSprite(BaseSprite):
     def update(self, dt):
         self.x += self.speed * dt
         self.rect.x = round(self.x)
+
+
+# rectangular collision check
+def collide_rect(a, b) -> bool:
+    return a.rect.colliderect(b.rect)
+
+
+# mask collision check
+# def collide_mask(a, b) -> bool:
+#     return pygame.sprite.collide_mask(a, b)
